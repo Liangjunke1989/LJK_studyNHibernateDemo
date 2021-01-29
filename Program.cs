c2382187aedfe10f4e8c0fffb1941e19d20d1877
梁俊可 <2583493258@qq.cpm>
2021-01-29 17:05:39 +0800

    20210129_建立mysql连接，成功读取数据。

--- Program.cs
+++ Program.cs
@@ -11,14 +11,29 @@ namespace LJK_CSharpLinkMySQLDemo_20210129
             MySqlConnection conn = new MySqlConnection(connectStr);//创建连接对象
             try
             {
-                conn.Open();//建立连接
-                Console.WriteLine("已经建立连接了！");
+                conn.Open(); //建立连接
+                
+                string sql = "select * from users";
+                MySqlCommand cmd = new MySqlCommand(sql,conn);
+                //cmd.ExecuteReader();//执行一些查询（读取数据）
+                //cmd.ExecuteNonQuery();//执行插入操作
+                //cmd.ExecuteScalar();//执行一些查询，返回单个查询的值
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("用户名为:{0},  密码为:{1},  注册日期为:{2}",reader[1],reader[2],reader[3]);
+                }
+                Console.WriteLine("\n已经成功建立连接了！");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-            conn.Close();//关闭连接
+            finally
+            {
+                conn.Close();//关闭连接
+            }
+           
             Console.ReadKey();
         }
     }
