488ae1c52ce1d85619e297baa14f9ac6c6d86f66
梁俊可 <2583493258@qq.cpm>
2021-01-29 16:41:19 +0800

    20210129_建立文件测试,NuGet安装引入所需要的mysql.data.dll文件，使用MySqlConnection建立连接

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
-﻿namespace LJK_CSharpLinkMySQLDemo_20210129
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace LJK_CSharpLinkMySQLDemo_20210129
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            string connectStr = "server=127.0.0.1;port=3306;database=MyGameDB;user=root;password=liang521;";
+            MySqlConnection conn = new MySqlConnection(connectStr);//创建连接对象
+            try
+            {
+                conn.Open();//建立连接
+                Console.WriteLine("已经建立连接了！");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            conn.Close();//关闭连接
+            Console.ReadKey();
         }
     }
 }
